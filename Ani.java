dc00d5ad2cd7322928aab7d7b6c499b079becc72
nithu33 <42604896+nithu33@users.noreply.github.com>
2018-08-23 20:58:59 +0530

    Add files via upload
    creating a abstract class and abstract method in this class

---
+++ Ani.java
@@ -0,0 +1,22 @@
+
+
+public abstract class Ani {                       //creating a abstract class
+	public void display()
+	{
+		System.out.println("the different types of animals sound:");          //creating object in extending class to print
+		System.out.println("woof ");
+		System.out.println("bark ");
+		System.out.println("screech");
+		System.out.println("growl ");
+		System.out.println("chimp ");
+		
+		
+	}
+ 	public abstract void show();                   //declaring a abstract member function
+
+
+}
+
+public class Ani {
+
+}
